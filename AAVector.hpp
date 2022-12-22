e53862ebb54fe0fc24cc8612a533e1c15acad895
Ahmedreda122 <ahmalolireda12@gmail.com>
2022-12-22 11:54:46 +0200

    Finishing my part

--- AAVector.hpp
+++ AAVector.hpp
@@ -18,7 +18,7 @@ public:
   {
   public:
 
-    using iterator_category = bidirectional_iterator_tag;
+    using iterator_category = random_access_iterator_tag;
     using difference_type   = ptrdiff_t;
     using value_type        = T;
     using pointer           = T*;
@@ -59,6 +59,19 @@ public:
       --(*this);
       return tmp;
     }
+
+    bool operator< (Iterator& it2) const {return (ptr < it2.ptr);}
+    bool operator<= (Iterator& it2) const {return (ptr <= it2.ptr);}
+
+    bool operator> (Iterator& it2) const  {return (ptr > it2.ptr);}
+    bool operator>= (Iterator& it2) const {return (ptr >= it2.ptr);}
+
+    Iterator operator+(const int& n) const {return Iterator(ptr + n);}
+    Iterator operator+=(const int& n) {ptr += n; return *this;}
+
+    Iterator operator-(const int& n) const {return Iterator(ptr - n);}
+    Iterator operator-=(const int& n) {ptr -= n; return *this;}
+
     friend bool operator== (const Iterator& p1, const Iterator& p2) { return p1.ptr == p2.ptr; };
     friend bool operator!= (const Iterator& p1, const Iterator& p2) { return p1.ptr != p2.ptr; };
 
@@ -68,53 +81,63 @@ public:
     // Constructors and Big 4 
     AAVector (int capacity = 0);
     // Initialize by specific capacity
-    // // No content is added, size = 0
-    // // Assign a default size value
+    // No content is added, size = 0
+    // Assign a default size value
     // AAVector (T* arr, int nitems);		// Initialize by n items from array 
     // AAVector (const AAVector& anotherVec);	// Initialize with a copy
-    // ~AAVector();			// Delete allocated memory
+    ~AAVector() {delete[] vecPtr;}		// Delete allocated memory
     // AAVector &operator=(const AAVector& anotherVec);  // Copy assignment  
     // AAVector &operator=(const AAVector&& anotherVec); // Move assignment 
 
-    // // Access operations 
+    // Access operations 
     T& operator[](int index); // Access item by reference 
-    // // Throw an exception if out of range 
+    // Throw an exception if out of range 
 
-
-    
     // Modifying operations 
     int push_back(T item); // Add item to end of vec & return # of items 
-            // Increase capacity of needed
-    T pop_back();     // Remove and return last element in vec 
-    void erase(Iterator it);        // Remove item at iterator
-    //             // Throw exception if invalid iter 
-    //void erase(Iterator it1, Iterator it2); // Remove items between 
-    // // iterator 1 <= iterator 2 otherwise do nothing
-    // // Throw exception if any iterator outside range
-    // void clear();     // Delete all vector content
+                          // Increase capacity of needed
+    T pop_back(); // Remove and return last element in vec 
+    void erase(Iterator it);  // Remove item at iterator
+    // Throw exception if invalid iter 
+    void erase(Iterator it1, Iterator it2); // Remove items between 
+    // iterator 1 <= iterator 2 otherwise do nothing
+    // Throw exception if any iterator outside range
+    void clear();     // Delete all vector content
     // void insert(Iterator it, T item);       // Insert item at iterator 
-    // // Throw exception if invalid 
+    // Throw exception if invalid 
 
-    // // Iterators 		// Supports *, + and ++ operations at least
-    // // Can use: typedef T* iterator 
-    // // Or u can use std::iterator so you can 
-    // // apply STL algorithms on AAVector
-    Iterator begin() const {return Iterator(vecPtr);}	// Return an iterator (T*) 
-    Iterator end() const {return Iterator(vecPtr + (size - 1));}	// Return an iterator (T*) 
+    // Iterators 		// Supports *, + and ++ operations at least
+    // Can use: typedef T* iterator 
+    // Or u can use std::iterator so you can 
+    // apply STL algorithms on AAVector
+    Iterator begin() const {AAVector<T>::Iterator it (vecPtr); return it;}	// Return an iterator (T*) 
+    Iterator end() const {AAVector<T>::Iterator it (vecPtr + (size - 1)); return it;}	// Return an iterator (T*) 
 
-    // // Comparison operations 
-    // bool operator==(const AAVector<T>&); // Return true if ==  
-    // bool operator< (const AAVector<T>&); // Compares item by item // Return true if first different item in this is < in other
+  // Comparison operations 
+  bool operator==(const AAVector<T>& vec2); // Return true if ==  
+  bool operator< (const AAVector<T>& vec2); // Compares item by item // Return true if first different item in this is < in other
 
-    // // Capacity operations 
-    int Size() const {return size;}    // Return current size of vec
-    int Capacity() const {return capacity;} // Return size of current allocated array  
-    //int resize();         // Relocate to bigger space
-    bool empty();         // Return true if size is 0
+  // Capacity operations 
+  int Size() const {return size;}    // Return current size of vec
+  int Capacity() const {return capacity;} // Return size of current allocated array  
+  void resize(int newSize);         // Relocate to bigger space
+  bool empty();         // Return true if size is 0
 
-    // // Friends 
-    // friend ostream& operator << (ostream& out, AAvector<T>)
-
+  // Friends
+  friend ostream& operator<<(ostream& out, const AAVector<T>& vec)
+  {
+    out << '{';
+    for (int i = 0; i < vec.size; i++)
+    {
+      if (i == vec.size - 1)
+      {
+        out << vec.vecPtr[i] << '}' << endl;
+        break;
+      }
+      out << vec.vecPtr[i] << ", ";
+    }
+    return out;
+  }
 };
 
 #endif
--- main.cpp
+++ main.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
 #include <iterator>
+#include <vector>
 #include "AAVector.imp"
 using namespace std;
 
 
 int main()
 {
-  AAVector<int> vec(5);
+  AAVector<int> vec(15);
   cout << vec.push_back(122) << endl;
   cout << vec.push_back(5) << endl;
   cout << vec.push_back(2) << endl;
@@ -29,6 +30,27 @@ int main()
   vec.erase(++vec.begin());
   cout << vec[1] << endl;
   vec.erase(++vec.end());
+
+  // vector<string> v(5);
+  // cout << v.size() << endl;
+  // v.resize(10);
+  // cout << v.size() << endl;
+  // cout << v[9];
+  AAVector<int> vec2(10);
+  cout << vec2.push_back(122) << endl;
+  cout << vec2.push_back(8) << endl;
+  cout << vec2.push_back(9) << endl;
+  cout << vec2.push_back(4) << endl;
+  cout << vec2.push_back(333) << endl;
+  cout << vec2.push_back(344) << endl;
+  cout << vec2.push_back(888) << endl;
+  AAVector<int>::Iterator it1 = vec2.begin();
+  advance(it1, 2);
+  
+  AAVector<int>::Iterator it2 = vec.end();
+  cout << vec2;
+  vec2.erase(it1, it2);
+  cout << vec2;
 }
 
 
